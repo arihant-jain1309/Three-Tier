eea25eb61c5eb4f8198dc12ec85bff443827903b
ARIHANT  JAIN <asjain1309@gmail.com>
2025-08-24 12:03:26 +0530

    Update app.py

--- notification-service/app.py
+++ notification-service/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
 
+
+@app.route("/notifications")
+def get_notifications():
+return jsonify([
+{"id":1, "message":"Order 1 shipped"},
+{"id":2, "message":"Payment pending for Order 2"}
+])
+
+
+if __name__ == "__main__":
+app.run(host="0.0.0.0", port=5005)
