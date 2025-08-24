6126aae73b427050e61856f47b9b0804799d868f
ARIHANT  JAIN <asjain1309@gmail.com>
2025-08-24 11:59:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,32 @@
+import React, { useEffect, useState } from 'react';
 
+function App() {
+  const [users, setUsers] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5001/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+    fetch("http://localhost:5002/products")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Three-Tier Microservices App</h1>
+      <h2>Users:</h2>
+      <ul>
+        {users.map(u => <li key={u.id}>{u.name}</li>)}
+      </ul>
+
+      <h2>Products:</h2>
+      <ul>
+        {products.map(p => <li key={p.id}>{p.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
