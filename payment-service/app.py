3ea30404b4abfb072aea430ee41770ba3d605348
ARIHANT  JAIN <asjain1309@gmail.com>
2025-08-24 12:02:58 +0530

    Update app.py

--- payment-service/app.py
+++ payment-service/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
 
+
+@app.route("/payments")
+def get_payments():
+return jsonify([
+{"id":1, "order_id":1, "status":"Paid"},
+{"id":2, "order_id":2, "status":"Pending"}
+])
+
+
+if __name__ == "__main__":
+app.run(host="0.0.0.0", port=5004)
