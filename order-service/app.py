6542efeefc74e1bb72ddefa6faf719af132951a0
ARIHANT  JAIN <asjain1309@gmail.com>
2025-08-24 12:02:27 +0530

    Update app.py

--- order-service/app.py
+++ order-service/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
 
+
+@app.route("/orders")
+def get_orders():
+return jsonify([
+{"id":1, "product":"Laptop", "user":"John"},
+{"id":2, "product":"Mouse", "user":"Jane"}
+])
+
+
+if __name__ == "__main__":
+app.run(host="0.0.0.0", port=5003)
